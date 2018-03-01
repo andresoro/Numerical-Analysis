46d48984dfe3d77921154d1b8552e89b9f878452
Andres Orozco <ao15@my.fsu.edu>
2018-03-01 14:56:34 -0500

    add bisection and newtons methods for root approximation

--- hw3/approx.py
+++ hw3/approx.py
@@ -10,3 +10,25 @@ def df(f, x):
     fx = f(compx + ih)
     return np.imag(fx)/h
 
+
+def bisection(f, a, b, tol, N):
+    i = 1 
+    fa = f(a)
+    while i <= N:
+        #compute Pi
+        p = a + ((b - a)/2)
+        fp = f(p)
+        if fp == 0 or (b-a)/2 < tol:
+            return p
+        i += 1
+
+        if fa*fp > 0:
+            a = p
+            fa = fp
+        else:
+            b = p
+
+def newton(f, x, tol):
+    while (abs(f(x) > tol)):
+        x = x - (f(x)/df(f, x))
+    return x
