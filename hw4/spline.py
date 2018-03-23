16e865e63d598eeb1bb4fefe1e7dd4228bac133b
Andres Orozco <ao15@my.fsu.edu>
2018-03-23 11:00:32 -0400

    fix array of coefficients

--- hw4/spline.py
+++ hw4/spline.py
@@ -11,12 +11,14 @@ A = np.array([
     [1, 0, 0, 0],
     [1, pih, pih**2, pih**3],
     [0, 1, pi, (3*(pih)**2)],
-    [0, 0, 2, 3*pi],
+    [0, 2, 3*pi, -2],
     [0, 1, 0, 0],
-    [0, 0, pi, 0]
+    [0, 0, pi, 3*pi]
 ])
 
 B = np.array([1, 0, 0, -1, 0, 0, 0, 0])
+
+
 
 
 S = np.linalg.lstsq(A, B)
@@ -29,7 +31,7 @@ a1, b1, c1, d1 = S1[0], S1[1], S1[2], S1[3]
 
 def spline(x):
     results = []
-    for i in x:
+    for i in x:  
         if i <= pih:
             results.append(
                 a0 + (b0*i) + (c0*(i**2)) + (d0*(i**3)) 
@@ -40,8 +42,8 @@ def spline(x):
             )
     return results
             
-x = np.linspace(0, 2*pi, 256, endpoint=True)
+x = np.linspace(0, pi, 256, endpoint=True)
 a = spline(x)
 
-plt.plot(x, np.cos(x))
+plt.plot(x, a)
 plt.show()
