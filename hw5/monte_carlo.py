61e096ed4dae926df0cb844cc7ea118a42f55818
Andres Orozco <ao15@my.fsu.edu>
2018-04-08 19:51:48 -0400

    add 1D monte carlo method

--- hw5/monte_carlo.py
+++ hw5/monte_carlo.py
@@ -1,1 +1,24 @@
 # Monte Carlo method 
+
+import random
+
+random.seed()
+
+def f(x):
+    return x**2 + 2
+
+def monte_carlo(f, N):
+    total = 0
+    for x in range(N - 1):
+        x = random.randint(-1, 1)
+        x = 2*(x-.5)
+        total = total + f(x)
+    return (2*total) / N
+
+exact = 14/3
+error = abs(monte_carlo(f, 100) - exact)
+total = monte_carlo(f, 100)
+
+print("approx: ", total)
+print("exact: ", exact)
+print("error: ", error)
