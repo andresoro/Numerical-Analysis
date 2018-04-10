57ef94c9430321381ee4ec3f59191a1ba0f6a051
Andres Orozco <ao15@my.fsu.edu>
2018-04-09 21:54:42 -0400

    add test func for multi gauss

--- hw5/multi_gauss.py
+++ hw5/multi_gauss.py
@@ -8,9 +8,18 @@ def multi_gauss(x, mu, cov):
     cov is a symmetric positive definite matrix
     """
 
-   numerator =  (-1/2) * ((x-mu).T.dot(np.linalg.inv(cov))).dot((x-mu))
-   denominator =  1 / ( ((2* np.pi)**(len(mu)/2)) * (np.linalg.det(cov)**(1/2)) )
+    numerator =  (-1/2) * ((x-mu).T.dot(np.linalg.inv(cov))).dot((x-mu))
+    denominator =  1 / ( ((2* np.pi)**(len(mu)/2)) * (np.linalg.det(cov)**(1/2)) )
 
-   numerator = np.exp(numerator)
+    numerator = np.exp(numerator)
 
-   return (numerator*denominator)
+    return (numerator*denominator)
+
+def test_multi_gauss():
+    x = np.array([[0], [0]])
+    mu = np.array([[0], [0]])
+    cov = np.eye(2)
+    print(multi_gauss(x, mu, cov))
+
+if __name__ == '__main__':
+    test_multi_gauss()
