e360afbaed8bb1b0c2764a7cec1c493db072106d
Andres Orozco <ao15@my.fsu.edu>
2018-04-09 21:50:27 -0400

    add multidimensional gauss distribution function

--- hw5/multi_gauss.py
+++ hw5/multi_gauss.py
@@ -1,4 +1,16 @@
 # Multi dimensional Gaussian distribution
 
-def multi_gauss(parameter_list):
-    pass
+import numpy as np
+
+def multi_gauss(x, mu, cov):
+    """
+    x and mu must be vectors with same dimensions
+    cov is a symmetric positive definite matrix
+    """
+
+   numerator =  (-1/2) * ((x-mu).T.dot(np.linalg.inv(cov))).dot((x-mu))
+   denominator =  1 / ( ((2* np.pi)**(len(mu)/2)) * (np.linalg.det(cov)**(1/2)) )
+
+   numerator = np.exp(numerator)
+
+   return (numerator*denominator)
