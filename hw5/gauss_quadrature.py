49e3d1b696c674a71b828ee6249a69ce465109da
Andres Orozco <ao15@my.fsu.edu>
2018-04-13 08:27:00 -0400

    add legendre polynomial definitions

--- hw5/gauss_quadrature.py
+++ hw5/gauss_quadrature.py
@@ -5,10 +5,37 @@
 import numpy as np
 from multi_gauss import multi_gauss
 
-def gauss_quadrature(a, b, m, n, p):
-    h1 = (b - a) / 2
-    h2 = (b + a) / 2
-    J = 0
+# recursive legendre polynomial
+def legendre(n, x):
+    x = np.array(x)
+    if n == 0:
+        return x*(0)+1
+    elif n == 1:
+        return x
+    else: 
+        return ((2*n-1)*x*legendre(n-1, x) - (n-1)*legendre(n-2, x))/n
 
-    for i in range(1, m):
-        Jx = 0
+# Derivative of legendre polynomials
+def Dlegendre(n, x):
+    x=np.array(x)
+    if n==0:
+        return x*0
+    elif n==1:
+        return x*0+1
+    else:
+        return (n/(x**2-1.0))*(x*Legendre(n,x)-Legendre(n-1,x))
+    
+# Roots of Legendre poly returns [x, y ] x = sample points, y = weights
+def LegendreRootsAndWeights(order):
+    return np.polynomial.legendre.leggauss(order)
+
+def GLQuadrature(func, order, a, b):
+    [xs, Ws] = LegendreRootsAndWeights(order)
+    return  (b-a)*0.5*sum(Ws*func((b-a)*0.5*xs + (b+a)*0.5))
+
+
+if __name__ == '__main__':
+    order = 3
+    
+    [ans, err] = GLQuadrature(multi_gauss, order, -1, 1)
+    print(ans)
--- hw5/multi_gauss.py
+++ hw5/multi_gauss.py
@@ -3,11 +3,16 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-def multi_gauss(x, mu, cov):
+def multi_gauss(x, mu=0, cov=np.eye(1)):
     """
     x and mu must be vectors with same dimensions
     cov is a symmetric positive definite matrix
     """
+
+    if not isinstance(x, np.ndarray):
+        x = np.array(x)
+    if not isinstance(mu, np.ndarray):
+        mu = np.array(mu)
 
     numerator =  (-1/2) * ((x-mu).T.dot(np.linalg.inv(cov))).dot((x-mu))
     denominator =  1 / ( ((2* np.pi)**(len(mu)/2)) * (np.linalg.det(cov)**(1/2)) )
@@ -17,9 +22,9 @@ def multi_gauss(x, mu, cov):
     return (numerator*denominator)
 
 def test_multi_gauss():
-    x = np.array([[0], [0]])
-    mu = np.array([[0], [0]])
-    cov = np.eye(2)
+    x = np.array([[0], [0], [0]])
+    mu = np.array([[0], [0], [0]])
+    cov = np.eye(3)
     print(multi_gauss(x, mu, cov))
 
 # vectorize takes in a domain of values and returns
@@ -28,11 +33,11 @@ def vectorize(domain, D):
     pass
 
 if __name__ == '__main__':
-    domain = np.linspace(-1, 1, 200)
+    domain = np.linspace(-10, 10, 200)
     mu = np.array([0])
     vec = [np.array([i]) for i in domain]
     ran = [multi_gauss(i, mu, np.eye(1)) for i in vec]
-
-    plt.plot(domain, ran)
-    plt.show()
+    test_multi_gauss()
+    #plt.plot(domain, ran)
+    #plt.show()
     
